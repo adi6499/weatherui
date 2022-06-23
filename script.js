1c5048f03a432a6e7f643e6e9392ae5b1124e0b2
adi6499 <83864698+adi6499@users.noreply.github.com>
2022-06-23 11:09:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
         name = document.querySelector(".name")
         country = document.querySelector(".country")
 
-
+    let blocks = document.querySelector(".blocks")
 
 
 
@@ -20,10 +20,14 @@
         set.addEventListener("click", locationData)
 
         async function locationData() {
+           
             let url = `https://api.openweathermap.org/data/2.5/weather?q=${loc.value}&appid=b4c40c9560455f7c1458b63bbfeb2560`;
+           try{
             let resp = await fetch(url)
+            
             let data = await resp.json()
-            console.log(data)
+          
+            
             description.innerHTML = data.weather[0].description
             visibility.innerHTML = data.visibility
             humidity.innerHTML = data.main.humidity + "%"
@@ -33,7 +37,16 @@
             country.innerHTML =data.sys.country  
             animationDelay()
             hello(Math.floor(data.main.temp - 274.14),data.name,data.weather[0].description)
-
+           }
+           catch(error){
+            description.innerHTML = "NO DATA Found"
+            visibility.innerHTML =  "NO DATA Found"
+            humidity.innerHTML =  "NO DATA Found"
+            speed.innerHTML =  "NO DATA Found"
+            temperature.innerHTML =  "NO DATA Found"
+            name.innerHTML= "NO DATA Found"
+            country.innerHTML = "NO DATA Found"
+           }
 
             
 
@@ -86,13 +99,13 @@
             contain.style.backgroundImage = `linear-gradient(${deg}deg,rgba(0, 0, 0, 0.854),rgba(0, 0, 0, 0.26)),url(./images/cloud${i}.jpg)`
             i++
 
-        }, 60000);
+        },1000*60);
 
         setInterval(() => {
             contain.style.backgroundImage = `linear-gradient(${deg}deg,rgba(0, 0, 0, 0.854),rgba(0, 0, 0, 0.26)),url(./images/cloud${i}.jpg)`
             deg++
 
-        }, 10);
+        }, 100);
 
 
 
@@ -100,6 +113,6 @@
             console.log(conditionName)
             setInterval(()=>{
                 new Notification(`${locationName} Temperature:${degree}degree \n Condition:${conditionName}`)
-            },30000)
+            },100000)
            
         }
